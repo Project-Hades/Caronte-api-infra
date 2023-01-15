ebb16692f5aa964e83312032ce53179effb1b028
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-15 19:27:08 -0300

    fix: display image from error message

--- caronte_infra/errors/database.py
+++ caronte_infra/errors/database.py
@@ -1,16 +1,8 @@
 class UndefinedDatabaseException(Exception):
     def __init__(self, error: str) -> None:
-        super().__init__("Undefined database.")
-        self.error = error
-
-    def __str__(self) -> str:
-        return f"{super().__str__()} {self.error}"
+        super().__init__(f"Undefined database. {error}")
 
 
 class UndefinedFieldConfiguration(Exception):
     def __init__(self, error: str) -> None:
-        super().__init__("Undefined field configuration.")
-        self.error = error
-
-    def __str__(self) -> str:
-        return f"{super().__str__()} {self.error}"
+        super().__init__(f"Undefined field configuration. {error}")
