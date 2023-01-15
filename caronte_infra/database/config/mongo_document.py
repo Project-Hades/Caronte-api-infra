c7e81fc8dd5e6e638b24e9447a3db6237b3068e2
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-01-15 18:38:43 -0300

    refactor: field configuration building

--- caronte_infra/database/config/mongo_document.py
+++ caronte_infra/database/config/mongo_document.py
@@ -6,6 +6,8 @@ from caronte_common.data.db_document import Document
 from caronte_common.interfaces.command import AsyncCommand
 from pymongo.collection import Collection
 
+from caronte_infra.errors.database import UndefinedFieldConfiguration
+
 
 class MongoConfigDocument(AsyncCommand[None]):
     def __init__(self, database: Collection, document: Document[Collection]) -> None:
@@ -15,13 +17,12 @@ class MongoConfigDocument(AsyncCommand[None]):
         )
 
     async def execute(self) -> Awaitable[None]:
-        _map = {
-            "create_index": lambda field_name, params: self.document.instance.create_index(
-                field_name, **params
-            )
-        }
         for field_config in self.document.field_config:
-            await _map[field_config.config_type](
-                field_name=field_config.field_name,
-                params=field_config.params,
-            )
+            if field_config.config_type == "create_index":
+                await self.document.instance.create_index(
+                    field_config.field_name, **field_config.params
+                )
+            else:
+                raise UndefinedFieldConfiguration(
+                    error="There are avaible this configurations: create_index"
+                )
